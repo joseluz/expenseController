203586d928a34ea0de8e6c84ddb722ba8585e633
Jose Luz <jose.rock@gmail.com>
2013-07-23 16:34:41 -0300

    - Removed AddExpenseFlyout and moved it content to MainPage. - Included Categories icon at Images.

--- ExpenseController.UI/MainPage.xaml.cs
+++ ExpenseController.UI/MainPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExpenseController.UI.Model;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -10,6 +11,7 @@ using Windows.UI.Xaml.Controls.Primitives;
 using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -38,7 +40,32 @@ namespace ExpenseController.UI
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            new AddExpenseFlyout().Open();
+            if (!addExpensePopUp.IsOpen)
+            {
+                BindCategoryList();
+                addExpensePopUp.HorizontalOffset = Window.Current.Bounds.Width - popupBorder.Width + 5;
+                popupBorder.Height = Window.Current.Bounds.Height + 10;
+                addExpensePopUp.IsOpen = true;
+            }
+        }
+
+        private void BackButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (addExpensePopUp.IsOpen)
+                addExpensePopUp.IsOpen = false;
+        }
+
+        public void BindCategoryList()
+        {
+            var baseUri = new Uri("ms-appx:///");
+            var categories = new List<ExpenseCategory>();
+            categories.Add(new ExpenseCategory() { Id = 1, Name = "Transporte", Image = new BitmapImage(new Uri(baseUri, "Images/bus-48.png")), });
+            categories.Add(new ExpenseCategory() { Id = 2, Name = "Farmacia", Image = new BitmapImage(new Uri(baseUri, "Images/apartment-48.png")) });
+            categories.Add(new ExpenseCategory() { Id = 3, Name = "Almoco" });
+            categories.Add(new ExpenseCategory() { Id = 4, Name = "Mercado" });
+            categories.Add(new ExpenseCategory() { Id = 5, Name = "Feira" });
+            categories.Add(new ExpenseCategory() { Id = 6, Name = "Moradia", Image = new BitmapImage(new Uri(baseUri, "Images/log_cabin-48.png")) });
+            categoriesListView.ItemsSource = categories;
         }
     }
 }
